c7fea39683777d705048e3738536bdf48f25db9e
MiterX <you@example.com>
2018-03-27 00:39:11 +0200

    make it better

---
+++ Key_secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'qUBMefCMklpZeqB1x201kkHlI',
+  consumer_secret:      'HjCc7DMqIYIcvS3CApOv1lbncEh6RAgSS8iRMnX5wYmfGzFWF7',
+  access_token:         '936951490631077888-xek7qSRuU5VUh7eJEbFR6EMR9fmcXSC',
+  access_token_secret:  'GZuGzW9APqNMRi18jucewouReeO39gIObxuUcauACl61J',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
